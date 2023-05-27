2ac402f1da371b7de6e677be00889bfc3d277639
Leandro Bove <leandrobove@hotmail.com>
2023-05-27 11:52:33 -0300

    add docker-compose and flyway configurations

--- src/main/resources/db/migration/V001__init-tables.sql
+++ src/main/resources/db/migration/V001__init-tables.sql
@@ -1,5 +1,5 @@
 create table client(
-    id uuid not null primary key,
+    id varchar(36) not null primary key,
     name varchar(255) not null,
     email varchar(255) not null,
     created_at timestamp not null,
@@ -8,20 +8,20 @@ create table client(
 );
 
 create table account(
-    id uuid not null primary key,
+    id varchar(36) not null primary key,
     balance decimal(15,2) not null,
     created_at timestamp not null,
     updated_at timestamp not null,
-    client_id uuid not null,
+    client_id varchar(36) not null,
     foreign key (client_id) references client(id)
 );
 
 create table transaction(
-    id uuid not null primary key,
+    id varchar(36) not null primary key,
     amount decimal(15,2) not null,
     created_at timestamp not null,
-    account_from_id uuid not null,
-    account_to_id uuid not null,
+    account_from_id varchar(36) not null,
+    account_to_id varchar(36) not null,
     foreign key(account_from_id) references account(id),
     foreign key(account_to_id) references account(id)
 );
