7e51ca61872b0819622859e0fd3a833dbcf19636
Leandro Bove <leandrobove@hotmail.com>
2023-06-01 14:14:37 -0300

    create ms-balance project

---
+++ ms-balance/src/main/java/com/github/leandrobove/msbalance/MsBalanceApplication.java
@@ -0,0 +1,13 @@
+package com.github.leandrobove.msbalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBalanceApplication.class, args);
+	}
+
+}
---
+++ ms-balance/src/test/java/com/github/leandrobove/msbalance/MsBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.leandrobove.msbalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
